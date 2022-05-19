d932ea6475264c31f6cf0d0b746843306ff1413d
Ferronique May <ferronique@bbd.co.za>
2022-05-19 19:08:23 +0200

    Added Booking Card. Fixed Responsives of invite card. Both card now accept variables as attributes

--- client/static/components/myInvites.js
+++ client/static/components/myInvites.js
@@ -3,56 +3,40 @@ class InvitesCard extends HTMLElement {
       super();
     }
     connectedCallback() {
-        // const partnerId = this.attributes.partnerId.value
-        // const partnerName = this.attributes.partnerName.value
-        // const partnerAge = this.attributes.partnerAge.value
-        // const partnerDisplayImage = this.attributes.partnerDisplayImage.value
-        // const partnerPronouns = this.attributes.partnerPronouns.value
-        // const partnerDistance = this.attributes.partnerDistance.value
-        // const partnerBio = this.attributes.partnerBio.value
+        const userId = this.attributes.userId.value
+        const userName = this.attributes.userName.value
+        const userAge = this.attributes.userAge.value
+        const userDisplayImage = this.attributes.userDisplayImage.value
+        const userPronouns = this.attributes.userPronouns.value
+        const userDistance = this.attributes.userDistance.value
+        const userBio = this.attributes.userBio.value
+        const eventTitle = this.attributes.eventTitle.value
+        const eventsDate = this.attributes.eventsDate.value
+        const eventsLocation = this.attributes.eventsLocation.value
+        const eventDetails = this.attributes.eventDetails.value
 
-      this.innerHTML = `<article class="eventCard">
-                        <profile-card
-                        partnerId='1'
-                        partnerName='Name'
-                        partnerAge='26'
-                        partnerPronouns='She/Her'
-                        partnerDistance='16'
-                        partnerBio='Lörem ipsum remynde mynt i premåra krojen. Klustersmitta viskap i deras i emmodern. Monoskapet vavis. Rägisk uberisering om jekrorade multifigisk. Prelån pretoliga poras. '
-                        partnerDisplayImage='static/images/profile-test.jpg'>
-                        </profile-card>
-                    <section>
-                        <summary class="event_details">
-                            <h2>Event Name</h2>
-                            <p>Event Location</p>
-                            <p>Event description</p>
-                        </summary>
-                        <button class="btn-peach">Accept</button>
-                        <button class="btn-blue">Decline</button>
-                    </section>
-                </article>
-                <article class="eventCard">
-                    <section class="profile_container">
-                        <profile-card
-                        partnerId='1'
-                        partnerName='Name'
-                        partnerAge='26'
-                        partnerPronouns='She/Her'
-                        partnerDistance='16'
-                        partnerBio='Lörem ipsum remynde mynt i premåra krojen. Klustersmitta viskap i deras i emmodern. Monoskapet vavis. Rägisk uberisering om jekrorade multifigisk. Prelån pretoliga poras. '
-                        partnerDisplayImage='static/images/profile-test.jpg'>
-                        </profile-card>
-                    </section>
-                    <section>
-                        <summary class="event_details">
-                            <h2>Event Name</h2>
-                            <p>Event Location</p>
-                            <p>Event description</p>
-                        </summary>
-                        <button class="btn-peach">Accept</button>
-                        <button class="btn-blue">Decline</button>
-                    </section>
-                </article>
+      this.innerHTML = `
+      <article class="eventCard">
+            <profile-card class='event-container'
+                partnerId='${userId}'
+                partnerName='${userName}'
+                partnerAge='${userAge}'
+                partnerPronouns='${userPronouns}'
+                partnerDistance='${userDistance}'
+                partnerBio='${userBio}'
+                partnerDisplayImage='${userDisplayImage}'>
+            </profile-card>
+            <section class='event-container'>
+                <summary class="event_details">
+                    <h2>${eventTitle}</h2>
+                    <p>${eventsDate}</p>
+                    <p>${eventsLocation}</p>
+                    <p>${eventDetails}</p>
+                </summary>
+                <button class="btn-peach">Accept</button>
+                <button class="btn-blue">Decline</button>
+            </section>
+        </article>
   `;
     }
   }
