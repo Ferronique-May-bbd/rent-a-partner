819425b67a4ad68c62d9f923ea967b28f590ed48
mothupir <mothupir@bbd.co.za>
2022-05-20 01:23:07 +0200

    connected add event

--- frontend/static/js/make-booking.js
+++ frontend/static/js/make-booking.js
@@ -1,3 +1,9 @@
+const calculateAge = (dateOfBirth) => {
+    const today = new Date().getFullYear();
+    const dob = new Date(dateOfBirth).getFullYear();
+    return today-dob;
+}
+
 function makeBooking(e) {
     e.preventDefault();
 
@@ -17,9 +23,39 @@ function makeBooking(e) {
     });
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+const getPartner = async(id) => {
+    try {
+        let res = await fetch(`http://localhost:5501/partner/${id}`);
+        return res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function putData() {
     var url = new URL(window.location.href);
-    console.log(url.searchParams.get("id"));
+    var id = url.searchParams.get("id");
+
+    var data = await getPartner(id);
+    let partner = `<profile-card
+                    partnerId='${data.UserId}'
+                    partnerName='${data.Name} ${data.Surname}'
+                    partnerAge='${calculateAge(data.DateOfBirth)}'
+                    partnerGender='${data.Gender}'
+                    partnerDistance='${data.Location}'
+                    partnerBio='${data.Bio}'
+                    partnerDisplayImage='static/img/${data.ProfilePicUrl}'
+                    partnerLanguage='${data.Language}'
+                    partnerRace='${data.Race}'
+                    partnerHeight='${data.Height} cm'>
+                </profile-card>`;
+                console.log(data);
+    document.getElementById("userDetails").innerHTML = partner;
+}
+
+putData();
+
+document.addEventListener("DOMContentLoaded", () => {
     var form = document.querySelector("form");
     form.addEventListener('submit', userLogin);
 });
